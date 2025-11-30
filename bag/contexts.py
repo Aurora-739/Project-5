33bcfa5547f26345d8b56c7886b5dcd392a97635
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-11-30 12:45:59 +0000

    user can now adjust quantity and remove items from the cart

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,34 +1,34 @@
-from django.conf import settings
 from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
-    """
-    Makes the shopping bag available across all templates.
-    """
-    bag = request.session.get('bag', {})
+
     bag_items = []
-    total = Decimal('0.00')
+    total = Decimal('0')
     product_count = 0
 
+    bag = request.session.get('bag', {})
+
     for item_id, quantity in bag.items():
-        # Get product info
-        from products.models import Product
-        product = Product.objects.get(pk=item_id)
-        total += product.price * quantity
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
         product_count += quantity
 
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
-            'line_total': product.price * quantity,
         })
+
+    grand_total = total  # no delivery yet since you said no delivery
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
-        'grand_total': total,  # no delivery
+        'grand_total': grand_total,
     }
 
     return context
