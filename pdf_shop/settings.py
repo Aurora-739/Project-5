d7f3204bf3b757ba0cff5b8b296a7677e66b2cc7
Aurora-739 <yfifej9y@students.codeinstitute.net>
2026-06-04 13:30:04 +0100

    update email settings in settings.py

--- pdf_shop/settings.py
+++ pdf_shop/settings.py
@@ -221,6 +221,12 @@ else:
 
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'http'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+
 # ----------------------
 # Messages
 # ----------------------
