5020b08a2e2087d49d189ce8b6e6d41223e84c0e
Aurora-739 <yfifej9y@students.codeinstitute.net>
2026-06-09 13:56:19 +0100

    Add Review model and migrations

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 #Products / models.py
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
@@ -29,3 +30,21 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    """Custom model for product reviews"""
+    RATING_CHOICES = [(i, i) for i in range(1, 6)]
+    
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('product', 'user')
+
+    def __str__(self):
+        return f'Review by {self.user.username} for {self.product.name}'
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,4 @@
+#Profile / models.py
 from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
