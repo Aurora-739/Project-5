9d23fc4f46f00abe5fee9c39365c65690388e3b3
Aurora-739 <yfifej9y@students.codeinstitute.net>
2026-06-09 15:41:39 +0100

    add wishlist urls to urls.py

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('<str:sku>/review/add/', views.add_review, name='add_review'),
     path('<str:sku>/review/<int:review_id>/edit/', views.edit_review, name='edit_review'),
     path('<str:sku>/review/<int:review_id>/delete/', views.delete_review, name='delete_review'),
+    path('wishlist/', views.wishlist, name='wishlist'),
+    path('<str:sku>/wishlist/add/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('<str:sku>/wishlist/remove/', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
